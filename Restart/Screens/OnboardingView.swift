c92f91e88850b43611da70f1d4ef0436e222f6bd
Zach Cervi <zachcervi@gmail.com>
2022-05-31 08:47:26 -0600

    built out Onboarding screen

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -11,14 +11,78 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Onboarding").font(.largeTitle)
-            Button(action : {
-                isOnboardingViewActive = false
-            }){
-                Text("Start")
-            }
-        }//: VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            
+            VStack(spacing: 20) {
+                //HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size:60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+It's not how much we give but how much love we put into giving
+""").font(.title3).fontWeight(.light).foregroundColor(.white).multilineTextAlignment(.center).padding(.horizontal, 10)
+                } // :HEADER
+                
+                //CENTER
+                ZStack {
+                    ZStack {
+                        Circle().stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle().stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                } //: CENTER
+                
+                Spacer()
+                //FOOTER
+                ZStack {
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }.foregroundColor(.white)
+                            .frame(width: 80, height: 80, alignment: .center)
+                            .onTapGesture {
+                                isOnboardingViewActive = false
+                            }
+                        Spacer()
+                    } //: HSTACK
+                    
+                }.frame(height: 80, alignment: .center).padding() //: FOOTER
+            }//: VSTACK
+        }//: ZSTACK
     }
 }
 
