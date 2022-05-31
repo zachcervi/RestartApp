799886e7337427e53dcb3d03f4eed454a90fa0e0
Zach Cervi <zachcervi@gmail.com>
2022-05-31 17:57:54 -0600

    added custom slider

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
     
     var body: some View {
         ZStack {
@@ -55,7 +57,7 @@ It's not how much we give but how much love we put into giving
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffset + 80)
                         Spacer()
                     }
                     HStack {
@@ -69,13 +71,27 @@ It's not how much we give but how much love we put into giving
                                 .font(.system(size: 24, weight: .bold))
                         }.foregroundColor(.white)
                             .frame(width: 80, height: 80, alignment: .center)
-                            .onTapGesture {
-                                isOnboardingViewActive = false
-                            }
+                            .offset(x: buttonOffset)
+                            .gesture(DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }.onEnded { _ in
+                                    if buttonOffset > buttonWidth / 2 {
+                                        buttonOffset = buttonWidth - 80
+                                        isOnboardingViewActive = false
+                                    } else {
+                                        buttonOffset = 0
+                                    }
+                                  
+                                    
+                                }
+                            )//: GESTURE
                         Spacer()
                     } //: HSTACK
                     
-                }.frame(height: 80, alignment: .center).padding() //: FOOTER
+                }.frame(width: buttonWidth, height: 80, alignment: .center).padding() //: FOOTER
             }//: VSTACK
         }//: ZSTACK
     }
